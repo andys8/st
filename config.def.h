73b8746d585bd2cb706156c4ae79c9b056cad837
andys8 <andys8@users.noreply.github.com>
2020-05-24 16:19:49 +0200

    Change cursor color if selected

--- config.def.h
+++ config.def.h
@@ -143,7 +143,7 @@ static const char *colorname[] = {
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 static unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
